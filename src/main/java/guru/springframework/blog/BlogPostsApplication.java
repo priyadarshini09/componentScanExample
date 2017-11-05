fa1a2f3ba12c478154751cbbea7f93ebe1b59ca4
Priyadarshini S <priyad920@gmail.com>
2017-11-05 18:47:49 +0530

    spring boot application

--- src/main/java/guru/springframework/blog/BlogPostsApplication.java
+++ src/main/java/guru/springframework/blog/BlogPostsApplication.java
@@ -10,35 +10,29 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+/*@Configuration
 @ComponentScan(basePackages = {"guru.springframework.blog.componentscan.example.demopackageA",
         "guru.springframework.blog.componentscan.example.demopackageD",
         "guru.springframework.blog.componentscan.example.demopackageE"},
-        basePackageClasses = DemoBeanB1.class)
+        basePackageClasses = DemoBeanB1.class)*/
+@SpringBootApplication
 public class BlogPostsApplication {
 
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new
-                AnnotationConfigApplicationContext();
-        try{
-            context.register(BlogPostsApplication.class);
-            context.refresh();
-            System.out.println("Spring ComponentScan Example "
-                    +context.getBean(BlogPostsApplication.class));
-            System.out.println("DemoBeanA " + context.getBean(DemoBeanA.class));
-            System.out.println("DemoBeanB1 " + context.getBean(DemoBeanB1.class));
-            System.out.println("DemoBeanD " + context.getBean(DemoBeanD.class));
-            System.out.println("DemoBeanE " + context.getBean(DemoBeanE.class));
-            System.out.println("Contains B2  " + context.containsBean("demoBeanB2"));
-            System.out.println("Contains C   " + context.containsBean("demoBeanC"));
-            System.out.println("Contains D   " + context.containsBean("demoBeanD"));
+        ApplicationContext context = SpringApplication.run(BlogPostsApplication.class,args);
+            System.out.println("Contains A  "+context.
+                    containsBeanDefinition("demoBeanA"));
+            System.out.println("Contains B2  " + context.
+                    containsBeanDefinition("demoBeanB2"));
+            System.out.println("Contains C   " + context.
+                    containsBeanDefinition("demoBeanC"));
+            System.out.println("Contains D   " + context.
+                    containsBeanDefinition("demoBeanD"));
 
-        }finally {
-            context.close();
-        }
     }
 }
